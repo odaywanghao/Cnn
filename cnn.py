01c4f1e145ebde1616734c47fb097242e95b0431
ev0 <evo_x@Rocketmail.com>
2015-05-12 19:43:43 -0400

    Added implementation files.

---
+++ cnn.py
@@ -0,0 +1,362 @@
+# Copyright (c) 2015 ev0, lautimothy
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import sys
+import numpy as np
+import scipy as sp
+import theano as thn
+import theano.tensor as tn
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import theano.tensor.nnet.conv as conv
+import theano.tensor.signal.downsample as downsample
+from copy import deepcopy
+from mlp import *
+from util import *
+from skimage.transform import downscale_local_mean as downsample
+from scipy.signal import convolve2d as conv2d
+from scipy.signal import correlate2d as corr2d
+
+
+def maxpool(data, factor):
+	"""
+	Perform max pooling of a given factor on data.
+	The image size must be divisible by the factor.
+
+	Args:
+	-----
+		data: A N x l x m1 x n1 array of feature maps.
+		factor: A tuple specifying the pooling factor.
+
+	Returns:
+	--------
+		An N x k x m2 x n2 array of feature maps.
+	"""
+	x = tn.dtensor4('x')
+	f = thn.function([x], downsample.max_pool_2d(x, factor))
+	return f(data)
+
+
+def upsample(data, factor):
+	"""
+	Get activations.
+	"""
+	return np.kron(data, np.ones(factor)) * (1.0 / np.prod(factor))
+
+
+def getActivations(data, factor):
+	"""
+	Upsample the errors for data by a given factor.
+
+	Args:
+	-----
+		errors: An N x k x m2 x n2 array of errors.
+		data: An N x l x m1 x n1 array of feature maps.
+		factor: A tuple repr. the pooling factor.
+
+	Returns:
+	--------
+		An N x l x m1 x n1 array of upsampled errors.
+	"""
+	x = tn.dtensor4('x')
+	f = thn.function([x], downsample.max_pool_2d_same_size(x, factor))
+	activations = f(data)
+	activations[np.where(activations != 0)] = 1
+	#Randomly select an activation where there are multiple in a receptive field.
+	N, k, m, n = activations.shape
+	activations = f(activations + (0.001 * np.random.randn(N, k, m, n)))
+	activations[np.where(activations != 0)] = 1
+	return activations
+
+
+def fastConv2d(data, kernel, convtype='valid'):
+	"""
+	Convolve data with the given kernel.
+
+	Args:
+	-----
+		data: A N x l x m2 x n2 array repr. data.
+		kernel: An k x l x m1 x n1 array repr. kernel.
+
+	Returns:
+	--------
+		A N x k x m x n array representing the output.
+	"""
+	d = tn.dtensor4('d')
+	k = tn.dtensor4('k')
+	f = thn.function([d, k], conv.conv2d(d, k, data.shape, kernel.shape, convtype))
+	return f(data, kernel)
+
+
+class ConvLayer():
+	"""
+	A Convolutional layer.
+	"""
+
+	def __init__(self, k, ksize, pfctr):
+		"""
+		Initialize convolutional layer.
+
+		Args:
+		-----
+			k: No. feature maps in layer.
+			ksize: Tuple repr. the size of a kernel.
+			pfctr: Pooling factor.
+		"""
+		self.pfctr = pfctr
+		self.kernels = (6.0 / (k * np.prod(ksize))) * np.random.randn(k, ksize[0], ksize[1])
+		self.bias = np.zeros((k, 1, 1))
+
+
+	def bprop(self, dE):
+		"""
+		Compute error gradients and return sum of error from output down
+		to this layer.
+
+		Args:
+		-----
+			dE: A N x k x m2 x n2 array of errors from prev layers.
+
+		Returns:
+		-------
+			A N x l x m1 x n1 array of errors.
+		"""
+		dEds = upsample(dE, self.pfctr) * sech2(self.maps + self.bias)
+
+		self.dEdb = np.sum(np.sum(np.average(dEds, axis=0), axis=1), axis=1).reshape(self.bias.shape)
+
+		dEds, x_sum = np.swapaxes(dEds, 0, 1), np.sum(self.x, axis=1)
+		#Correlate
+		for i in xrange(x_sum.shape[0]):
+			x_sum[i] = np.rot90(x_sum[i], 2)
+		self.dEdw = fastConv2d(x_sum[np.newaxis, :], dEds)[0] / dE.shape[0]
+
+		dEds, kernels = np.swapaxes(dEds, 0, 1), np.zeros(self.kernels.shape)
+		#Correlate
+		for i in xrange(kernels.shape[0]):
+			kernels[i] = np.rot90(self.kernels[i], 2)
+		self.dEdx = fastConv2d(dEds, kernels[np.newaxis, :], 'full')
+		return np.tile(self.dEdx, (1, self.x.shape[1], 1, 1))
+
+
+	def update(self, lr):
+		"""
+		Update the weights in this layer.
+
+		Args:
+		-----
+			lr: Learning rate.
+		"""
+		self.kernels = self.kernels - (lr * self.dEdw)
+		self.bias = self.bias - (lr * self.dEdb)
+		#printMatrix(self.bias[0])
+
+
+	def feedf(self, data):
+		"""
+		Perform a forward pass on the input data.
+
+		Args:
+		-----
+			data: An N x l x m1 x n1 array of input plains.
+
+		Returns:
+		-------
+			A N x k x m2 x n2 array of output plains.
+		"""
+		self.x = data
+		N, l, m1, n1 = self.x.shape
+		k, m, n = self.kernels.shape
+		self.maps = fastConv2d(self.x, np.tile(self.kernels.reshape(k, 1, m, n), (1, l, 1, 1)))
+
+		return downsample(np.tanh(self.maps + self.bias), (1, 1, self.pfctr[0], self.pfctr[1]))
+
+
+class Cnn():
+	"""
+	Convolutional neural network class.
+	"""
+
+	def __init__(self, layers):
+		"""
+		Initialize network.
+
+		Args:
+		-----
+			layers: Dict of fully connected and convolutional layers arranged heirarchically.
+		"""
+		self.layers = deepcopy(layers["fully-connected"]) + deepcopy(layers["convolutional"])
+		self.div_x_shape = None
+		self.div_ind = len(layers["fully-connected"])
+
+
+	def train(self, train_data, train_label, valid_data, valid_label, test_data, test_label, params):
+		"""
+		Train the convolutional neural net on the training and validation set.
+
+		Args:
+		-----
+			train_data 	:	no_imgs x img_length x img_width array of images.
+			valid_data 	:	no_imgs x img_length x img_width array of images.
+			train_label :	k x no_imgs binary array of image class labels.
+			valid_label :	k x no_imgs binary array of image class labels.
+			params 		:	A dictionary of training parameters.
+		"""
+
+		for i in xrange(200):
+
+			pred = self.predict(train_data)#[i].reshape(1, m, n))
+			label = train_label#np.zeros(pred.shape)
+			#label[:, train_label[i]] = 1
+
+			self.backprop(pred - label) #Note: dw = w - lr * dEdw
+			self.update(params)
+
+			train_clfn = self.classify(pred)
+			valid_clfn = self.classify(self.predict(valid_data))
+
+			#print train_clfn.shape
+			#print valid_clfn.shape
+
+			train_ce = (float(label.shape[0]) - np.sum(np.where(train_clfn == label, train_clfn, 0))) / float(label.shape[0])
+			valid_ce = (float(valid_label.shape[0]) - np.sum(np.where(valid_clfn == valid_label, valid_clfn, 0))) / float(valid_label.shape[0])
+
+			print '\rIteration:' + "{:10.2f}".format(i) + ' Train MCE:' + "{:10.2f}".format(train_ce) + ' Valid MCE:' + "{:10.2f}".format(valid_ce)
+			if i != 0 and i % 100 == 0:
+  				print '\n'
+
+  		test_clfn = self.classify(self.predict(test_data))
+  		test_ce = mce(test_clfn, test_label)
+  		print '\rTest MCE:' + "{:10.2f}".format(test_ce)
+
+  		return 0
+
+	def backprop(self, dE):
+		"""
+		Propagate the error gradients through the network.
+
+		Args:
+		-----
+			dE: A no_imgs x k_classes array of error gradients.
+		"""
+		error = dE.T
+		for layer in self.layers[0 : self.div_ind]:
+			error = layer.bprop(error)
+
+		#Reshape output from fully-connected layer.
+		N, k, m, n = self.div_x_shape
+		error = error.T.reshape(N, k, m, n)
+
+		for layer in self.layers[self.div_ind:]:
+			error = layer.bprop(error)
+
+
+	def predict(self, imgs):
+		"""
+		Return the probability distribution over the class labels for
+		the given images.
+
+		Args:
+		-----
+			data: A no_imgs x img_length x img_width array.
+
+		Returns:
+		-------
+			A no_imgs x k_classes array.
+		"""
+		#TODO: Modify for RGB images
+		N, m, n = imgs.shape
+		data = imgs.reshape(N, 1, m, n)
+
+		for i in xrange(len(self.layers) - 1, self.div_ind - 1, -1):
+			data = self.layers[i].feedf(data)
+
+		#Reshape output of convolutional layer.
+		self.div_x_shape = data.shape
+		data = data.reshape(data.shape[0], np.prod(data.shape[1:])).T
+
+		for i in xrange(self.div_ind - 1, -1, -1):
+			data = self.layers[i].feedf(data)
+
+		return data.T
+
+
+	def update(self, params):
+		"""
+		Update the network weights.
+
+		Args:
+		-----
+			params: Training parameters.
+		"""
+		for layer in self.layers:
+			layer.update(params['learn_rate'])
+
+
+	def classify(self, prediction):
+		"""
+		Peform binary classification on the class probabilities.
+
+		Args:
+		-----
+			prediction: An N x k array of class probabilities.
+
+		Returns:
+		--------
+			An N x k array of binary class assignments.
+		"""
+		N, k = prediction.shape
+		clasfn = np.zeros((N, k))
+
+		for row in xrange(N):
+			ind = np.where(prediction[row] == np.amax(prediction[row]))[0][0]
+			#prediction[row, :] = np.zeros(k)
+			clasfn[row, ind] = 1
+
+		return clasfn
+
+
+def testMnist(filename):
+	"""
+	Test cnn using all the mnist digits.
+
+	Args:
+	-----
+		filename: Name of file containing mnist digits.
+	"""
+	print "Loading data..."
+	data = np.load(filename)
+	train_data = data['train_data'][0:10000]
+	valid_data = data['valid_data'][0:1000]
+	test_data = data['test_data']
+	train_label = data['train_label'][0:10000]#.reshape(1000, 1)
+	valid_label = data['valid_label'][0:1000]#.reshape(1000, 1)
+	test_label = data['test_label']#.reshape(10000, 1)
+
+	print "Initializing network..."
+	layers = {
+		"fully-connected": [PerceptronLayer(10, 150, "softmax"), PerceptronLayer(150, 256, "tanh")],
+		# Ensure size of output maps in preceeding layer
+		# is equals to the size of input maps in next layer.
+		"convolutional": [ConvLayer(16, (5,5), (2, 2)), ConvLayer(6, (5,5), (2, 2))]
+		#"convolutional": [ConvLayer(16, (5,5), (8,8), (2,2), 6), ConvLayer(6, (5,5), (24,24), (2,2), 1)]
+	}
+	cnn = Cnn(layers)
+	print "Training network..."
+	cnn.train(train_data, train_label, valid_data, valid_label, test_data, test_label, {'learn_rate': 0.1})
+
+
+if __name__ == '__main__':
+
+	testMnist('data/cnnMnist.npz')
---
+++ mlp.py
@@ -0,0 +1,359 @@
+# Copyright (c) 2015 ev0, lautimothy
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import numpy as np
+from copy import deepcopy
+from util import *
+
+
+def sigmoid(data):
+	"""
+	Run The sigmoid activation function over the input data.
+
+	Args:
+	----
+		data : A k x N array.
+
+	Returns:
+	-------
+		A k x N array.
+	"""
+	return 1 / (1 + np.exp(-data))
+
+
+def softmax(data):
+	"""
+	Run the softmax activation function over the input data.
+
+	Args:
+	----
+		data : A k x N array.
+
+	Returns:
+	-------
+		A k x N array.
+	"""
+	k, N = data.shape
+	e = np.exp(data)
+	return e/np.sum(e, axis=0).reshape(1, N)
+
+
+def sech2(data):
+	"""
+	Find the hyperbolic secant function over the input data.
+
+	Args:
+	-----
+		data : A k x N array.
+
+	Returns:
+	--------
+		A k x N array.
+	"""
+	return np.square(1 / np.cosh(data))
+
+
+def cross_entropy(preds, labels):
+    """
+    Compute the cross entropy over the predictions.
+
+    Args:
+    -----
+        preds : An N x k array of class predicitions.
+        labels : An N x k array of class labels.
+    
+    Returns:
+    --------
+        The cross entropy.
+    """
+    N, k = preds.shape
+
+    if k == 1: #sigmoid
+        return -np.mean(labels * np.log(preds) + (1 - labels) * np.log(1 - preds))
+    
+    return -np.sum(np.sum(labels * np.log(preds), axis=1).reshape(N, 1))
+
+
+def mce(preds, labels):
+    """
+    Compute the mean classification error over the predictions.
+
+    Args:
+    ----
+        preds : An N x k array of class predictions.
+        labels: An N x k array of class labels.
+    
+    Returns:
+    --------
+        The mean classification error.
+    """
+    N, l = labels.shape
+    return np.sum(preds != labels)/float(N * l)
+
+
+class PerceptronLayer():
+	"""
+	A perceptron layer.
+	"""
+
+	def __init__(self, no_outputs, no_inputs, outputType="sigmoid"):
+		"""
+		Initialize fully connected layer.
+
+		Args:
+		-----
+			no_outputs: No. output classes.
+			no_inputs: No. input features.
+			outputType: Type of output ('sum', 'sigmoid', 'tanh' or 'softmax')
+		"""
+		self.o_type = outputType
+		if outputType == "sigmoid" or outputType == "tanh":
+			self.w = (6.0/(no_outputs + no_inputs)) * np.random.randn(no_outputs, no_inputs)
+		else:
+			self.w = 0.01 * np.random.randn(no_outputs, no_inputs)
+		self.b = 0.01 * np.random.randn(no_outputs, 1)
+		if no_outputs <= 2:
+			self.o_type = "sigmoid"
+
+
+	def bprop(self, dEds):
+		"""
+		Compute gradients and return sum of error from output down
+		to this layer.
+
+		Args:
+		-----
+			dEdx: A no_output x N array of errors from prev layers.
+
+		Return:
+		-------
+			A no_inputs x N array of input errors.
+		"""
+		if self.o_type == 'sigmoid':
+			dEdo = dEds
+			z = sigmoid(np.dot(self.w, self.x) + self.b)
+			dods = z * (1 - z)
+			dEds = dEdo * dods
+		elif self.o_type == 'tanh':
+			dEdo = dEds
+			dods = sech2(np.dot(self.w, self.x) + self.b)
+			dEds = dEdo * dods 
+
+		self.dEdw = np.dot(dEds, self.x.T)
+		self.dEdb = np.sum(dEds, axis=1).reshape(self.b.shape)
+		self.dEdx = np.dot(dEds.T, self.w).T
+		
+		return self.dEdx
+
+
+	def update(self, lr):
+		"""
+		Update the weights in this layer.
+		"""
+		m, N = self.x.shape
+		self.w = self.w - (lr * (self.dEdw / N))
+		self.b = self.b - (lr * (self.dEdb / N))
+
+
+	def feedf(self, data):
+		"""
+		Perform a forward pass on the input data.
+
+		Args:
+		-----
+			data: An no_inputs x N array.
+
+		Return:
+		-------
+			A no_outputs x N array.
+		"""
+		self.x = data
+		s = np.dot(self.w, self.x) + self.b
+
+		if self.o_type == "sum":
+			return s
+		elif self.o_type == "tanh":
+			return np.tanh(s)
+		elif self.o_type == "softmax": 
+			return softmax(s)
+
+		return sigmoid(s)
+
+
+class Mlp():
+	"""
+	A multilayer perceptron.
+	"""
+
+	def __init__(self, layers):
+		"""
+		Initialize the mlp.
+
+		Args:
+		-----
+			layers: List of mlp layers arranged heirarchically.
+		"""
+		self.layers = deepcopy(layers)
+
+
+	def train(self, train_data, train_target, valid_data, valid_target, test_data, test_target, hyperparameters):
+		"""
+		Train the mlp on the training set and validation set using the provided
+		hyperparameters.
+
+		Args:
+		----
+			train_data 	:	no_instance x no_features matrix.
+			train_target :	no_instance x k_class matrix.
+			valid_data 	:	no_instance x no_features matrix.
+			valid_target :	no_instance x k_class matrix.array shuffle numpy
+			hyperparameters :	A dictionary of training parameters.
+		"""
+		N, m1 = train_data.shape
+		N, m2 = train_target.shape
+
+		# Train the network with batch 'cos
+		# online too erratic & mini-batch too much work.
+		epochs = hyperparameters['epochs']
+
+		for epoch in xrange(epochs):
+
+			self.backprop(self.predict(train_data) - train_target)
+			self.update(hyperparameters)
+
+			#Measure network's performance.
+			train_class = self.classify(self.predict(train_data))
+			valid_class = self.classify(self.predict(valid_data))
+			ce_train = mce(train_class, train_target)
+			ce_valid = mce(valid_class, valid_target)
+			print '\rEpoch' + "{:10.2f}".format(epoch) + ' Train MCE:' + "{:10.2f}".format(ce_train) + ' Validation MCE:' + "{:10.2f}".format(ce_valid)
+			if epoch != 0 and epoch % 100 == 0:
+  				print '\n'
+
+  		test_class = self.classify(self.predict(test_data))
+		ce_test = mce(test_class, test_target)
+  		print '\r Test MCE:' + "{:10.2f}".format(ce_test)
+
+  		return 0
+
+
+  	def backprop(self, dEds):
+  		"""
+  		Propagate the error gradients through the network.
+
+  		Args:
+  		-----
+  			dEds: Error gradient w.r.t WX.
+  		"""
+  		error = dEds.T
+  		for i in xrange(len(self.layers)):
+  			error = self.layers[i].bprop(error)
+
+
+  	def update(self, parameters):
+  		"""
+  		Update the network weights using the training
+  		parameters.
+
+  		Args:
+  		-----
+  			parameters: Training parameters.
+  		"""
+  		for layer in self.layers:
+  			layer.update(parameters['learn_rate'])
+
+
+  	def predict(self, data):
+  		"""
+  		Return the posterior probability distribution of data
+  		over k classes.
+
+  		Args:
+  		-----
+  			data: An N x k array of input data.
+
+  		Returns:
+  		-------
+  			An N x k array of posterior distributions.
+  		"""
+  		x = data.T
+  		for i in xrange(len(self.layers) - 1, 0, -1):
+  			x = self.layers[i].feedf(x)
+
+  		return self.layers[0].feedf(x).T
+
+
+  	def classify(self, prediction):
+		"""
+		Peform classification using the class predictions of the classifier.
+		"""
+		m, n = prediction.shape
+
+		if n == 1:
+			for row in xrange(m):
+				if prediction[row, 0] < 0.5:
+					prediction[row, 0] = 0
+				else:
+					prediction[row, 0] = 1 
+		else:
+			for row in xrange(m):
+				i = 0
+				for col in xrange(n):
+					if prediction[row, col] > prediction[row, i]:
+						i = col
+				prediction[row, :] = np.zeros(n)
+				prediction[row, i] = 1
+
+		return prediction
+
+
+	def saveModel(self):
+		"""
+		Save the neural network model.
+		"""
+		#TODO: Implement.
+		pass
+
+
+	def loadModel(self, model):
+		"""
+		Load a model for this neural network.
+		"""
+		#TODO: Implement this later.
+		pass
+
+
+def testmlp(filename):
+  	"""
+  	Test mlp with mnist 2 and 3 digits.
+
+  	Args:
+  	-----
+    	filename: Name of the file for 2 and 3.
+  	"""
+	data = np.load(filename)
+	input_train = np.hstack((data['train2'], data['train3']))
+	input_valid = np.hstack((data['valid2'], data['valid3']))
+	input_test = np.hstack((data['test2'], data['test3']))
+	target_train = np.hstack((np.zeros((1, data['train2'].shape[1])), np.ones((1, data['train3'].shape[1]))))
+	target_valid = np.hstack((np.zeros((1, data['valid2'].shape[1])), np.ones((1, data['valid3'].shape[1]))))
+	target_test = np.hstack((np.zeros((1, data['test2'].shape[1])), np.ones((1, data['test3'].shape[1]))))
+
+	mlp = Mlp([PerceptronLayer(1, 10), PerceptronLayer(10, 256, "tanh")])
+	mlp.train(input_train.T, target_train.T, input_valid.T, target_valid.T, input_test.T, target_test.T, {'learn_rate': 0.1, 'epochs': 1600})
+
+
+if __name__ == '__main__':
+
+	testmlp('data/digits.npz')
